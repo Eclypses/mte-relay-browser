218fdb66d0b19c34615f83e871881f2ef50bcb03
Trevor Blackman <trevor.blackman@eclypses.com>
2024-03-05 05:40:33 -0700

    remove console.logs

--- src/mte/cache/cache.ts
+++ src/mte/cache/cache.ts
@@ -2,18 +2,9 @@ const cache = new Map();
 
 export function setCacheItem(key: string, value: any): void {
   cache.set(key, value);
-  // log out all cache items
-  Object.keys(cache).forEach((key) => {
-    console.log(key, cache.get(key));
-  });
 }
 
 export function getCacheItem<T>(key: string): T {
-  // log out all cache items
-  console.clear();
-  cache.forEach((value, key) => {
-    console.log(key, value);
-  });
   return cache.get(key);
 }
 
